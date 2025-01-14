6618547e2a018b4ae6a6417348c5b47c4247fabe
Colin-de-Seroux <77831261+Colin-de-Seroux@users.noreply.github.com>
2025-01-14 22:05:28 +0100

    :sparkles: Mise en place du script de prédictions

---
+++ yolo/utils.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+def export_model(model_name: str = "yolov8n", format: str = "onnx", device : str = "cpu", image_size : int = 640) -> str:
+    """
+    Exportation du modèle YOLO dans un certain format.
+
+    :param model_name : Nom du modèle à exporter.
+    :param format : Format d'exportation du modèle.
+    :param device : Périphérique sur lequel le modèle doit être exporté (GPU -> 0, CPU -> cpu, NVDIA Jetson -> dla:0 / dla:1).
+    :param image_size : Taille de l'image d'entrée du modèle.
+    """
+
+    model = YOLO(f"./saved/{model_name}.pt")
+
+    return model.export(format=format, device=device, imgsz=image_size)
